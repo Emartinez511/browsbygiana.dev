a23b0b2daa45bb9b1079d923f449bfe5d7d6a1e3
Emartinez511 <mrblonde9993@gmail.com>
2016-10-21 14:49:51 -0500

    added new homepage and images.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,122 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>Hello World</h1>
+
+
+
+<div class="row featurette landing_background">
+    <div class="col-md-5 col-md-offset-1">
+        <h2 class="featurette-heading">Welcome</h2>
+            <p class="lead">
+                I started my "love affair" with brows while attending The Aveda Institute at San Antonio
+                . After graduation, I became a licensed esthetician & started my career at K.Charles & Co. There I was able to build
+                a steady brow based clientele. Soon after, I accepted an offer with Benefit San Francisco Cosmetics as an "Arch Expert".
+                My "fuller shaped" brows became my signature. Fast forward 3 years & after major life changes, the dream of owning my brow
+                studio came true on 10/30/2012. Brows by Giana is dedicated to my beautiful nephews Elijah & Avi. XO.
+            </p>
+    </div>
+    <div class="col-md-5 col-md-offset-1">
+        <a href="http://www.genbook.com/bookings/slot/reservation/30189523"><img class="featurette-image img-responsive center-block" src="img/booknow_button.png" alt="book now image"></a>
+    </div>
+</div>
+
+<hr class="featurette-divider">
+
+<div class="brow_gals" id="second_feature">
+
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4 text-center">
+            <h2 class="featurette-heading">The Brow Gals</h2>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-3 col-md-offset-2 text-center">
+            <img class="img-responsive center-block" src="img/gianaframe.png" />
+            <h3>Giana</h3>
+        </div>
+        <div class="col-md-3 col-md-offset-2 text-center">
+            <img class="img-responsive center-block" src="img/brooklynframe.png" />
+            <h3>Brooklyn</h3>
+        </div>
+    </div>
+
+</div>
+<div class="reviews_segment">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4 text-center">
+            <h2 class="reviews_heading">Reviews</h2>
+            <img src="img/stars.png" />
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3">
+            <h3 class="text-center">The best!</h3>
+            <blockquote>
+                <p>
+                    “I come all the way from Dallas, TX to have Giana work her magic on my brows. She is the Anastasia to my Kim K.”
+                </p>
+                <p class="pull-right"><small>Meghan G</small></p>
+            </blockquote>
+        </div>
+
+        <div class="col-md-3 col-md-offset-1">
+            <h3 class="text-center">She's amazing</h3>
+            <blockquote>
+                <p>
+                    "Brooklyn is an artist and definitely knows what she is doing... Words of advice: Stop getting your brows done at nail salons!! Go to the experts.”
+                </p>
+                <p class="pull-right"><small>Priscilla</small></p>
+            </blockquote>
+        </div>
+
+        <div class="col-md-3 col-md-offset-1">
+            <h3 class="text-center">Best brow girls</h3>
+            <blockquote>
+                <p>
+                    "Giana and Brooklyn have both done my brows and let me tell you they are the best team! Love the way my brows look after each visit. Thanks girls”
+                </p>
+                <p class="pull-right"><small>Vanessa M</small></p>
+            </blockquote>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4 text-center">
+            {{-- <p>
+                Click Below for More Reviews
+            </p> --}}
+            <button type="button" class="btn btn-warning" id="reviews_btn" href="http://www.genbook.com/bookings/slot/reservation/30189523/reviews/">REVIEWS</button>
+        </div>
+    </div>
+</div>
+<div class="location_segment">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4 text-center">
+            <h2>Location</h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3 text-center">
+            <p>
+                <h4>We are located in Treusdell Salon & Spa in Suite 111</h3>
+                    11106 Huebner Rd Suite #111 SAN ANTONIO,TX 78230
+                </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4 text-center">
+            <iframe width="400" height="250" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJL_tLEDJnXIYRkP2zg1_0YYE&key=AIzaSyAx7VbchvbSpJT03UqHAU3j3tOz3fJNMSM" allowfullscreen></iframe>
+        </div>
+    </div>
+</div>
+
+
+
+          <!-- FOOTER -->
+          <footer>
+            <p class="pull-right"><a href="#">Back to top</a></p>
+            <p>&copy; 2016 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+          </footer>
+
+        </div><!-- /.container -->
 @stop
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <div class="row header">
+        <div class="col-xs-4 col-xs-offset-4">
+            <img class="img-responsive"id="header_img" src="img/logo_header.png" alt="browlogo"/>
+        </div>
+        <div class="col-xs-1 col-md-offset-1">
+            <img class="img-responsive center-block" id="instagram" src="img/instagram_icon.png" alt="instagram_logo"/>
+        </div>
+        <div class="col-xs-1">
+            <img class="img-responsive center-block" id="facebook" src="img/facebook.png" alt="facebook_logo"/>
+        </div>
+    </div>
+</div>
